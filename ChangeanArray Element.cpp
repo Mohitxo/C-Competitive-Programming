2b1a088dc3109ee2a83555777dafbc98b5583e20
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-03-04 12:03:49 +0530

    Change an Array Element.cpp

---
+++ ChangeanArray Element.cpp
@@ -0,0 +1,13 @@
+// Change an Array Element
+// To change the value of a specific element, refer to the index number -
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cars[4] = {"mohit","rohit","sohit","moni","ruchi"};
+  names[0] = "Opel";
+  cout << names[0];
+  return 0;
+}
