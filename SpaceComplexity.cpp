24090632c5a149f46b85eb61e994a1d7dcfac788
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-03-08 21:21:30 +0530

    Space Complexity
    Space Complexity - 
    The space complexity of an algorithm or a computer program is the amount of memory space required to solve an instance of the computational problem as a function
    of characteristics of the input. It is the memory required by an algorithm until it executes completely.

---
+++ SpaceComplexity.cpp
@@ -0,0 +1,22 @@
+// Space Complexity - 
+// The space complexity of an algorithm or a computer program is the amount of memory space required to solve an instance of the computational problem as a function
+// of characteristics of the input. It is the memory required by an algorithm until it executes completely.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+//   this is a variable storing a value having a time complex of O(1)
+  int a;
+  int b=5;
+
+//  an array storing a value of data having a time complexity of O(n)
+  int n=100;
+  int ar[n];
+  
+//  an 2D array having a time complexity of big O of O(n^2)
+  int arr[n][n];
+  
+// if we add all the three than the time complexity will be 
+//    O(1)+ O(n)+O(n^2)---->O(n^2)
+}
