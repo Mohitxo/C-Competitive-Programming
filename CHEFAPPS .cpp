3c9f13fd138afec26dc627a7526fd97c6c2ec337
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-06-29 23:45:35 +0530

    Add files via upload

---
+++ CHEFAPPS .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int s,x,y,z;
+	    cin>>s>>x>>y>>z;
+	    if((x+y+z)<=s){
+	        cout<<'0'<<endl;
+	    }
+	    
+	    else if((x+y+z)>=s){
+	        if((x+z)<=s){
+	            cout<<'1'<<endl;
+	        }
+	        else{
+	            cout<<'2'<<endl;
+	        }
+	    }
+	    
+	}
+	return 0;
+}
