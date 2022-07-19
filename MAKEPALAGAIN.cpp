72c0c6e7df59cf79c7766b9f04a1769c6a16eb82
Mohit Panjikar <90394406+Mohitxo@users.noreply.github.com>
2022-07-19 23:39:16 +0530

    Add files via upload

---
+++ MAKEPALAGAIN.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    string s;
+	    cin>>s;
+	    
+	    string a,b;
+	    
+	    for(int i=0; i<n; i+=2)
+	    a += s[i];
+	    
+	    for(int i=1; i<n; i+=2)
+	    b += s[i];
+	    
+	    sort(a.begin(),a.end());
+	    sort(b.begin(),b.end());
+	    
+	    if(a==b)
+	    cout<<"Yes"<<endl;
+	    else
+	    cout<<"No"<<endl;
+	    }
+	return 0;
+}
